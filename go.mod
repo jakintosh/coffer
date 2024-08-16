1e2ea33b2929e534bfea4ec5b597900cc20d85e0
Jak Tiano <jak33@mac.com>
2024-08-15 20:27:51 -0400

    initial commit: checkoutSession and webook handler

---
+++ go.mod
@@ -0,0 +1,5 @@
+module git.sr.ht/~jakintosh/salary
+
+go 1.22.6
+
+require github.com/stripe/stripe-go/v79 v79.7.0 // indirect
---
+++ server.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stripe/stripe-go/v79"
+	"github.com/stripe/stripe-go/v79/checkout/session"
+	"github.com/stripe/stripe-go/v79/webhook"
+	// "github.com/stripe/stripe-go/v79/price"
+	// portalsession "github.com/stripe/stripe-go/v79/billingportal/session"
+	"encoding/json"
+	"html"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/createCheckoutSession", createCheckoutSession)
+	http.HandleFunc("/webhooks", webhooks)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func createCheckoutSession(w http.ResponseWriter, r *http.Request) {
+	println("hit /createCheckoutSession")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8007")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// Set your secret key. Remember to switch to your live secret key in production.
+	// See your keys here: https://dashboard.stripe.com/apikeys
+	stripe.Key = "sk_test_..."
+
+	params := &stripe.CheckoutSessionParams{
+		Mode: stripe.String(string(stripe.CheckoutSessionModeSubscription)),
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			{
+				Price:    stripe.String("price_..."),
+				Quantity: stripe.Int64(1),
+			},
+		},
+		UIMode:    stripe.String(string(stripe.CheckoutSessionUIModeEmbedded)),
+		ReturnURL: stripe.String("https://example.com/checkout/return?session_id={CHECKOUT_SESSION_ID}"),
+	}
+
+	result, err := session.New(params)
+	if err != nil {
+		println("checkout session error")
+		json.NewEncoder(w).Encode(err)
+	} else {
+		println("successful checkout session")
+		json.NewEncoder(w).Encode(result)
+	}
+}
+
+func webhooks(w http.ResponseWriter, req *http.Request) {
+
+	// read payload
+	const MaxBodyBytes = int64(65536)
+	req.Body = http.MaxBytesReader(w, req.Body, MaxBodyBytes)
+	payload, err := io.ReadAll(req.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading request body: %v\n", err)
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+
+	// verify signature and construct event
+	signature := req.Header.Get("Stripe-Signature")
+	endpointSecret := "whsec_..."
+	event, err := webhook.ConstructEvent(payload, signature, endpointSecret)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error verifying webhook signature: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest) // Return a 400 error on a bad signature
+		return
+	}
+
+	// unmarshal the event data into an appropriate struct depending on its type
+	switch event.Type {
+	case "payment_intent.succeeded":
+		var paymentIntent stripe.PaymentIntent
+		err := json.Unmarshal(event.Data.Raw, &paymentIntent)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		handlePaymentIntent(paymentIntent)
+
+	case "customer.subscription.created",
+		"customer.subscription.paused",
+		"customer.subscription.resumed",
+		"customer.subscription.deleted",
+		"customer.subscription.updated":
+
+		var subscription stripe.Subscription
+		err := json.Unmarshal(event.Data.Raw, &subscription)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		handleSubscription(subscription)
+
+	default:
+		fmt.Fprintf(os.Stderr, "Unhandled event type: %s\n", event.Type)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func handlePaymentIntent(payment stripe.PaymentIntent) {
+	fmt.Fprintf(os.Stdout, "successful payment intent: %d", payment.AmountReceived)
+}
+func handleSubscription(subscription stripe.Subscription) {
+	fmt.Fprintf(os.Stdout, "RECEIVED SUBSCRIPTION\n%d", subscription.BillingCycleAnchor)
+}
