8a9e049b238c32dd69ed1ee252958a4c43eccfb9
Jak Tiano <310173+jakintosh@users.noreply.github.com>
2025-08-04 20:31:00 -0400

    added: allow serve options via cli

--- cmd/coffer/serve.go
+++ cmd/coffer/serve.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"git.sr.ht/~jakintosh/coffer/internal/api"
 	"git.sr.ht/~jakintosh/coffer/internal/database"
@@ -12,16 +13,63 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const (
+	DB_FILE_PATH          = "/var/lib/coffer"
+	PORT                  = "8080"
+	CORS_ALLOWED_ORIGINS  = "http://localhost:80"
+	CREDENTIALS_DIRECTORY = "/etc/coffer"
+)
+
+func resolveOption(
+	i *cmd.Input,
+	opt string,
+	env string,
+	def string,
+) string {
+	if v := i.GetParameter(opt); v != nil && *v != "" {
+		return *v
+	}
+	if v := os.Getenv(env); v != "" {
+		return v
+	}
+	return def
+}
+
 var serveCmd = &cmd.Command{
 	Name: "serve",
 	Help: "run the coffer server",
+	Options: []cmd.Option{
+		{
+			Long: "db-file-path",
+			Type: cmd.OptionTypeParameter,
+			Help: "database file path",
+		},
+		{
+			Long: "port",
+			Type: cmd.OptionTypeParameter,
+			Help: "port to listen on",
+		},
+		{
+			Long: "cors-allowed-origins",
+			Type: cmd.OptionTypeParameter,
+			Help: "comma-separated allowed origins",
+		},
+		{
+			Long: "credentials-directory",
+			Type: cmd.OptionTypeParameter,
+			Help: "credentials directory",
+		},
+	},
 	Handler: func(i *cmd.Input) error {
+		dbPath := resolveOption(i, "db-file-path", "DB_FILE_PATH", DB_FILE_PATH)
+		port := ":" + resolveOption(i, "port", "PORT", PORT)
+		originsStr := resolveOption(i, "cors-allowed-origins", "CORS_ALLOWED_ORIGINS", CORS_ALLOWED_ORIGINS)
+		var origins []string
+		if originsStr != "" {
+			origins = strings.Split(originsStr, ",")
+		}
 
-		dbPath := readEnvVar("DB_FILE_PATH")
-		port := fmt.Sprintf(":%s", readEnvVar("PORT"))
-		origins := readEnvVarList("CORS_ALLOWED_ORIGINS")
-
-		credsDir := readEnvVar("CREDENTIALS_DIRECTORY")
+		credsDir := resolveOption(i, "credentials-directory", "CREDENTIALS_DIRECTORY", CREDENTIALS_DIRECTORY)
 		stripeKey := loadCredential("stripe_key", credsDir)
 		endpointSecret := loadCredential("endpoint_secret", credsDir)
 		apiKey := loadCredential("api_key", credsDir)
